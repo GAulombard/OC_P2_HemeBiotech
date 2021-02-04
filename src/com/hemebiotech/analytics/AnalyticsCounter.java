f6a8a98995f2d468f82b287b8a14a3f7b5f74e22
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-02-04 16:10:00 +0100

    Javadoc update

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,37 +1,113 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 
 import static java.util.Collection.*;
 
+/**
+ * Allows, from a text file, to read, sort alphabetically, and write a new file
+ *
+ * @author geoffrey aulombard
+ */
+
+
 public class AnalyticsCounter {
+    /**
+     * data is a list of String elements
+     *
+     */
+    private List<String> data;
+    /**
+     *a Map that contains String keys and value
+     */
+    private Map symptomsOccurrence = new HashMap();
+    /**
+     *a Map that contains String keys and Long value organized alphabetically form the symptomsOccurence Map
+     * @see Map symptomsOccurence
+     */
+    private TreeMap<String, Long> map = new TreeMap<String, Long>();
 
-	public static void main(String args[]) throws Exception {
+    /**
+     *Constructor
+     *
+     * @param data
+     *      a list of String from a text file that use the ReadSymptomDataFromFile class
+     * @see ReadSymptomDataFromFile
+     *
+     */
 
-        HashMap hm = new HashMap();
-        int occurence = 0;
-        ReadSymptomDataFromFile dataInput = new ReadSymptomDataFromFile("symptoms.txt");
-		List<String> symptomsList = dataInput.GetSymptoms();
+    //----CONSTRUCTOR----
+    public AnalyticsCounter(List<String> data) {
 
-		//Sorting the symptoms list alphabetically
-		symptomsList.sort(Comparator.naturalOrder());
+        this.data = data;
 
-        Set<String> mySet = new HashSet<String>(symptomsList);
-        List<String> sortedList = new ArrayList<String>(mySet);
+    }
 
+    //----METHODS----
+    /**
+     *this method allow to write the list from the data List String, in the terminal
+     */
+    public void reading() {
 
-        for (int i = 0 ; i < sortedList.size() ; i++) {
-            hm.put(sortedList.get(i),Collections.frequency(symptomsList,sortedList.get(i)));
+        System.out.println("\n-------------- READING SYMPTOMS LIST ----------------\n");
+        System.out.println(data);
+
+    }
+    /**
+     *this method allow to re organize the list from data ReadSymptomDataFromFile.
+     *
+     * <p>It will re organize alphabetically and count the occurrence number and put all in a TreeMap.</p>
+     */
+    public void sorting(){
+
+        Set<String> noDuplicateSet = new HashSet<String>(data); // Delete duplicates
+        List<String> noDuplicateList = new ArrayList<String>(noDuplicateSet); // new list without duplicates
+
+        for (String symptom : noDuplicateList) { // fill the Map with symptoms(key) and occurrences(value)
+            symptomsOccurrence.put(symptom,Collections.frequency(data,symptom));
         }
 
-        hm.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+        map.putAll(symptomsOccurrence);
 
-		// -->> Create WriteFileFromMap function
-		FileWriter dataOutput = new FileWriter ("result.txt");
-		dataOutput.close();
+        System.out.println("\n-------------- READING SORTED SYMPTOMS LIST ----------------\n");
 
-	}
+        symptomsOccurrence.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+
+    }
+    /**
+     *allow to write in a file the result of the sorting() method.
+     *<p>If the file doesn't exist, a new file named "results.out" will be created.</p>
+     *
+     * @throws IOException if I/O operations failed or interrupt
+     */
+    public void saving() throws IOException {
+
+        sorting();
+        System.out.println("\n*** saving sorted symptoms list in \"../results.out\" file.................................");
+
+        try {
+
+            File results = new File("results.out");
+            BufferedWriter writer = new BufferedWriter(new FileWriter (results));
+            for(Map.Entry<String, Long> entry : map.entrySet()) {
+
+                writer.write(entry.getKey() + " = " + entry.getValue());
+                writer.newLine();
+                writer.flush();
+
+            }
+
+            writer.close();
+            System.out.println("* file saved !\n");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
--- src/com/hemebiotech/analytics/Main.java
+++ src/com/hemebiotech/analytics/Main.java
@@ -1,2 +1,19 @@
-package com.hemebiotech.analytics;public class Main {
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+
+        ReadSymptomDataFromFile dataInput = new ReadSymptomDataFromFile("symptoms.txt");
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(dataInput.GetSymptoms());
+
+
+        analyticsCounter.reading();
+        analyticsCounter.sorting();
+        analyticsCounter.saving();
+
+
+    }
 }
